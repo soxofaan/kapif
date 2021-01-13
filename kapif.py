cd390294e0690e27febb69bac8747ddafa490492
Stefaan Lippens <soxofaan@gmail.com>
2021-01-13 23:06:47 +0100

    Start using sqlite db (very simple POC)

--- kapif.py
+++ kapif.py
@@ -1,6 +1,8 @@
 import asyncio
+import contextlib
 import logging
 import os
+import sqlite3
 import time
 
 from fastapi import FastAPI
@@ -11,24 +13,48 @@ log = logging.getLogger(__name__)
 
 app = FastAPI()
 
-load_db = []
+config = {
+    "database": "kapif.db",
+    "poll_sleep": 10,
+}
+
+
+# TODO: encapsulate DB stuff better.
+@contextlib.contextmanager
+def db():
+    con = sqlite3.connect(config["database"])
+    yield con
+    con.commit()
+    con.close()
+
+
+def db_setup():
+    with db() as con:
+        tables = set(row[0] for row in con.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall())
+        log.info(f"db_setup: tables in db: {tables}")
+        if "cpu_load" not in tables:
+            log.info("Creating table cpu_load")
+            con.execute("CREATE table cpu_load(ts INT, load1 REAL, load5 REAL, load15 REAL)")
+            con.commit()
 
 
 async def poll_load():
-    global load_db
     log.info("Start polling CPU load")
     while True:
         # TODO: log "still polling" message from time to time
         timestamp = int(time.time())
         cpu_load = os.getloadavg()
-        load_db.append((timestamp, cpu_load))
+        with db() as con:
+            con.execute("INSERT INTO cpu_load VALUES (?,?,?,?)", (timestamp,) + cpu_load)
         # TODO: make sleep delay configurable
-        await asyncio.sleep(60)
+        await asyncio.sleep(config["poll_sleep"])
 
 
 @app.on_event("startup")
 async def on_startup():
     logging.basicConfig(level=logging.INFO)
+
+    db_setup()
 
     log.info("Set up poll loop.")
     asyncio.ensure_future(poll_load())
@@ -44,7 +70,12 @@ async def index():
 
 @app.get("/cpu_load")
 def get_cpu_load():
+    with db() as con:
+        loads = [
+            (row[0], row[1:])
+            for row in con.execute("SELECT * FROM cpu_load").fetchall()
+        ]
     return {
-        "loads": load_db,
+        "loads": loads,
         "series": ["load1", "load5", "load15"]
     }
